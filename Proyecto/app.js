f039749222d757a3d063e718d49738bd4076836e
Fatima <maria.cozzetto@alumnos.frgp.utn.edu.ar>
2021-09-18 12:51:29 -0300

    Agrego condición if

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -2,3 +2,10 @@ let edad= prompt('Por favor, ingrese su edad.')
 console.log(edad)
 
 alert(`La edad que usted ingresó es ${edad}`)
+
+if (edad<18){
+    alert('Usted debe tener más de 18 para continuar en esta página.');
+}
+else{
+    alert('Como ested es mayor de edad, puede continuar en esta página.')
+}
