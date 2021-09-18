d32e4412f41c17c291b4b3307858812733fc07bf
Fatima <maria.cozzetto@alumnos.frgp.utn.edu.ar>
2021-09-18 13:00:29 -0300

    MOD JS: modifico el if

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -4,8 +4,8 @@ console.log(edad)
 alert(`La edad que usted ingresó es ${edad}`)
 
 if (edad>=18){
-    alert('Usted debe tener más de 18 para continuar en esta página.');
+    alert('Como ested es mayor de edad, puede continuar en esta página.');
 }
 else{
-    alert('Como ested es mayor de edad, puede continuar en esta página.')
+    alert('Usted debe tener más de 18 para continuar en esta página.');
 }
